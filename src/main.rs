bb082588ce76507a38b2c4cc5c7c51e5b2244134
nikolaoslibero <nikolaos@violation.studio>
2025-04-01 07:01:27 -0400

    feat: add level spawning system
    Implemented a new system to spawn the initial game level, including loading a GLTF scene and setting ambient light brightness.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(window_plugin()))
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_systems(Startup, spawn_level)
         .run();
 }
 
@@ -18,4 +19,19 @@ fn window_plugin() -> WindowPlugin {
         }),
         ..default()
     }
+}
+
+#[expect(clippy::needless_pass_by_value, reason = "Rust/Bevy hate me.")]
+fn spawn_level(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut ambient_light: ResMut<AmbientLight>,
+) {
+    ambient_light.brightness = 100.0;
+
+    commands.spawn((
+        SceneRoot(asset_server.load(GltfAssetLabel::Scene(0).from_asset("elevator-room.gltf"))),
+        AsyncSceneCollider::default(),
+        RigidBody::Fixed,
+    ));
 }
