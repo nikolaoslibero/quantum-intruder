cb09729e4b5d0bbccd8ca4ae67eaaa7ec9ef7b56
nikolaoslibero <nikolaos@violation.studio>
2025-04-01 05:47:12 -0400

    feat: set up Bevy application with titled window
    Initialize a Bevy application with a window titled "Quantum Intruder" and integrated the Rapier physics plugin.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
+use core::num::NonZero;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(
+            window_plugin()
+        ))
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        .run();
+}
+
+fn window_plugin() -> WindowPlugin {
+    WindowPlugin {
+        primary_window: Some(Window {
+            title: "Quantum Intruder".to_owned(),
+            desired_maximum_frame_latency: NonZero::new(1u32),
+            ..default()
+        }),
+        ..default()
+    }
 }
