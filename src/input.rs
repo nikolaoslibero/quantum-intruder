fd1f7653102552aecf3c83c4d4dcce049158932a
nikolaoslibero <nikolaos@violation.studio>
2025-04-04 20:34:58 -0400

    refactor: implement input handling for camera movement and cursor locking
    Introduced a new Input resource to manage camera movement deltas and cursor lock state. Updated camera control systems to utilize the new Input structure, enhancing clarity and separation of concerns in the input handling logic.

--- src/input.rs
+++ src/input.rs
@@ -1,20 +1,39 @@
-use core::f32::consts::FRAC_PI_2;
-
-use crate::player::Player;
 use bevy::{input::mouse::AccumulatedMouseMotion, prelude::*, window::CursorGrabMode};
 pub struct InputPlugin;
 
 impl Plugin for InputPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<CursorLockState>().add_systems(
-            Update,
-            (cursor_lock_state, camera_pitch_control, camera_yaw_control),
-        );
+        app.init_resource::<Input>()
+            .add_systems(Update, (cursor_lock_state, camera_delta));
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Default, Resource)]
-enum CursorLockState {
+#[derive(Resource, Default)]
+pub struct Input {
+    camera_delta: Vec2,
+    cursor_lock_state: CursorLockState,
+}
+
+impl Input {
+    pub fn camera_moved(&self) -> bool {
+        self.camera_delta.length_squared() > 0.0
+    }
+
+    pub const fn camera_pitch_delta(&self) -> &f32 {
+        &self.camera_delta.y
+    }
+
+    pub const fn camera_yaw_delta(&self) -> &f32 {
+        &self.camera_delta.x
+    }
+
+    pub const fn cursor_lock_state(&self) -> &CursorLockState {
+        &self.cursor_lock_state
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Default)]
+pub enum CursorLockState {
     #[default]
     Free,
     Locked,
@@ -45,64 +64,16 @@ impl CursorLockState {
 #[expect(clippy::needless_pass_by_value, reason = "Bevy convention")]
 fn cursor_lock_state(
     mouse_buttons: Res<ButtonInput<MouseButton>>,
-    mut cursor_lock_state: ResMut<CursorLockState>,
+    mut input: ResMut<Input>,
     mut window: Single<&mut Window>,
 ) {
     if mouse_buttons.just_pressed(MouseButton::Right) {
-        cursor_lock_state.toggle();
-        cursor_lock_state.apply_to_bevy_window(&mut window);
+        input.cursor_lock_state.toggle();
+        input.cursor_lock_state.apply_to_bevy_window(&mut window);
     }
 }
 
 #[expect(clippy::needless_pass_by_value, reason = "Bevy convention")]
-fn camera_pitch_control(
-    mut camera_transforms: Query<&mut Transform, With<Camera3d>>,
-    mouse_motion: ResMut<AccumulatedMouseMotion>,
-    cursor_state: Res<CursorLockState>,
-) {
-    if *cursor_state == CursorLockState::Free || mouse_motion.delta.length_squared() <= 0.0 {
-        return;
-    }
-
-    for mut transform in &mut camera_transforms {
-        apply_mouse_pitch(&mut transform, mouse_motion.delta.y);
-    }
-}
-
-#[expect(clippy::float_arithmetic, reason = "Transform rotation")]
-fn apply_mouse_pitch(camera_transform: &mut Transform, pitch_delta: f32) {
-    const MOUSE_SENSITIVITY: f32 = -0.0005;
-    const PITCH_LIMIT: f32 = FRAC_PI_2 - f32::EPSILON;
-
-    let (current_yaw, current_pitch, current_roll) =
-        camera_transform.rotation.to_euler(EulerRot::YXZ);
-
-    let new_pitch = pitch_delta
-        .mul_add(MOUSE_SENSITIVITY, current_pitch)
-        .clamp(-PITCH_LIMIT, PITCH_LIMIT);
-
-    camera_transform.rotation =
-        Quat::from_euler(EulerRot::YXZ, current_yaw, new_pitch, current_roll);
-}
-
-#[expect(clippy::needless_pass_by_value, reason = "Bevy convention")]
-fn camera_yaw_control(
-    mut player_transforms: Query<&mut Transform, With<Player>>,
-    mouse_motion: ResMut<AccumulatedMouseMotion>,
-    cursor_state: Res<CursorLockState>,
-) {
-    if *cursor_state == CursorLockState::Free || mouse_motion.delta.length_squared() <= 0.0 {
-        return;
-    }
-
-    for mut transform in &mut player_transforms {
-        apply_mouse_yaw(&mut transform, mouse_motion.delta.x);
-    }
-}
-
-#[expect(clippy::float_arithmetic, reason = "Transform rotation")]
-fn apply_mouse_yaw(camera_transform: &mut Transform, yaw_delta: f32) {
-    const MOUSE_SENSITIVITY: f32 = -0.0005;
-
-    camera_transform.rotate_y(yaw_delta * MOUSE_SENSITIVITY);
+fn camera_delta(mouse_motion: ResMut<AccumulatedMouseMotion>, mut input: ResMut<Input>) {
+    input.camera_delta = mouse_motion.delta;
 }
--- src/player.rs
+++ src/player.rs
@@ -1,3 +1,6 @@
+use core::f32::consts::FRAC_PI_2;
+
+use crate::input::{CursorLockState, Input};
 use bevy::prelude::*;
 use bevy_rapier3d::prelude::*;
 
@@ -5,7 +8,9 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_player);
+        app.add_systems(Startup, spawn_player)
+            .add_systems(Update, camera_pitch_control)
+            .add_systems(Update, player_yaw_control);
     }
 }
 
@@ -44,3 +49,54 @@ impl Default for PlayerBundle {
 
 #[derive(Component)]
 pub struct Player;
+
+#[expect(clippy::needless_pass_by_value, reason = "Bevy convention")]
+fn camera_pitch_control(
+    mut camera_transforms: Query<&mut Transform, With<Camera3d>>,
+    input: Res<Input>,
+) {
+    if *input.cursor_lock_state() == CursorLockState::Free || !input.camera_moved() {
+        return;
+    }
+
+    for mut transform in &mut camera_transforms {
+        apply_mouse_pitch(&mut transform, *input.camera_pitch_delta());
+    }
+}
+
+#[expect(clippy::float_arithmetic, reason = "Transform rotation")]
+fn apply_mouse_pitch(camera_transform: &mut Transform, pitch_delta: f32) {
+    const MOUSE_SENSITIVITY: f32 = -0.0005;
+    const PITCH_LIMIT: f32 = FRAC_PI_2 - f32::EPSILON;
+
+    let (current_yaw, current_pitch, current_roll) =
+        camera_transform.rotation.to_euler(EulerRot::YXZ);
+
+    let new_pitch = pitch_delta
+        .mul_add(MOUSE_SENSITIVITY, current_pitch)
+        .clamp(-PITCH_LIMIT, PITCH_LIMIT);
+
+    camera_transform.rotation =
+        Quat::from_euler(EulerRot::YXZ, current_yaw, new_pitch, current_roll);
+}
+
+#[expect(clippy::needless_pass_by_value, reason = "Bevy convention")]
+fn player_yaw_control(
+    mut player_transforms: Query<&mut Transform, With<Player>>,
+    input: Res<Input>,
+) {
+    if *input.cursor_lock_state() == CursorLockState::Free || !input.camera_moved() {
+        return;
+    }
+
+    for mut transform in &mut player_transforms {
+        apply_mouse_yaw(&mut transform, *input.camera_yaw_delta());
+    }
+}
+
+#[expect(clippy::float_arithmetic, reason = "Transform rotation")]
+fn apply_mouse_yaw(camera_transform: &mut Transform, yaw_delta: f32) {
+    const MOUSE_SENSITIVITY: f32 = -0.0005;
+
+    camera_transform.rotate_y(yaw_delta * MOUSE_SENSITIVITY);
+}
